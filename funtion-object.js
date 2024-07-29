0986edce9385990e1d72a11a8ba85297e640dea8
meekailkhan <meekailkhan123aar@gmail.com>
2024-07-29 11:44:47 +0530

    hard part oop

--- funtion-object.js
+++ funtion-object.js
@@ -1,6 +1,8 @@
 function multiplyByTwo(num){
     return num * 2;
 }
+// in the javascsript when we create a function its not only function its both function and object 
+// function give us an empty object we can added key values using dote notation and javascript reffer to use this object
 
 multiplyByTwo.score = 13;
 multiplyByTwo.Name = "Abcd"
