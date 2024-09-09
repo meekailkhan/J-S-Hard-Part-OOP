d73ad8441c0b7ee722740a2e6ff0612731b979b9
pc <meekailkhan123aar@gmail.com>
2024-09-09 15:24:41 +0530

    hard parts of oop

--- constructor-approch.js
+++ constructor-approch.js
@@ -15,3 +15,20 @@ const user1 = new UserCreator("Phil",5);
 const user2 = new UserCreator("Tim",4);
 
 user2.sayName() // I am Tim
+
+function PaidUserCreator(paidName,paidScore,accountBalance){
+    UserCreator.call(this,paidName,paidScore);
+    // UserCreator.apply(this,[paidName,paidScore])
+
+    this.accountBalance = accountBalance;
+}
+
+PaidUserCreator.prototype = Object.create(UserCreator.prototype);
+PaidUserCreator.prototype.increamentBalance = function(){
+    this.accountBalance++
+}
+
+const paidUser1 = new PaidUserCreator("Alyssa",8,25);
+
+paidUser1.sayName()
+console.log(paidUser1.name)
